13f434808b944e1e62168cdaf690cbef84362f05
wpramz <williampramono7@gmail.com>
2024-04-16 22:34:38 -0700

    .Env file setup

--- src/firebase.js
+++ src/firebase.js
@@ -4,18 +4,11 @@
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { initializeApp} from 'firebase/app';
 import { getFirestore, Timestamp, FieldValue } from 'firebase/firestore';
+import 'dotenv/config'
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: "AIzaSyA0covIKtMSOvVk6sRUTaQ0se6yR-KSzoo",
-  authDomain: "ranked-voting-9fd69.firebaseapp.com",
-  projectId: "ranked-voting-9fd69",
-  storageBucket: "ranked-voting-9fd69.appspot.com",
-  messagingSenderId: "79816687579",
-  appId: "1:79816687579:web:e116afb3217fb5644531fe",
-  measurementId: "G-S74VDQ399G"
-};
+const firebaseConfig = JSON.parse(process.env.FIREBASE_CONFIG)
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
